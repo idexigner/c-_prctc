bae8c1e0c382d229e806e1633190136bd9af682d
Faraz Ahmed <farazahmed34296@gmail.com>
2018-03-28 15:51:57 +0500

    More programs added on Tecloud

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Practice
 {
@@ -14,7 +15,8 @@ namespace Practice
             Console.WriteLine("\n\n\nEnter the Respective Number");
             Console.WriteLine("1-Even/Odd\t\t2-break num and sum\t3-Reverse Number");
             Console.WriteLine("4-String to Char array\t5-Num of Time\t\t6-Reverse String");
-            Console.WriteLine("7-Upper and Lower Case\t8-Prime or Not");
+            Console.WriteLine("7-Upper and Lower Case\t8-Prime or Not\t\t9-reating file");
+            Console.WriteLine("10-Reading From file/t11-writing in file");
             int input = Convert.ToInt32(Console.ReadLine());
 
             //EVEN ODD
@@ -104,46 +106,77 @@ namespace Practice
             else if(input==8)
             {
                 int num,i,ctr=0;
-	
-	Console.Write("\n\n");
-    Console.Write("Check whether a given number is prime or not:\n");
-    Console.Write("-----------------------------------------------");
-    Console.Write("\n\n");  	
-	
-    Console.Write("Input  a number: ");
-    num= Convert.ToInt32(Console.ReadLine());	
-    for(i=2;i<=num/2;i++){
-        if(num % i==0){
-         ctr++;
-            break;
-        }
-    }
-   if(ctr==0 && num!= 1)
-        Console.Write("{0} is a prime number.\n",num);
-   else
-      Console.Write("{0} is not a prime number\n",num);
+
+                Console.Write("\n\n");
+                Console.Write("Check whether a given number is prime or not:\n");
+                Console.Write("-----------------------------------------------");
+                Console.Write("\n\n");
+
+                Console.Write("Input  a number: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                for (i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        ctr++;
+                        break;
+                    }
+                }
+                if (ctr == 0 && num != 1)
+                    Console.Write("{0} is a prime number.\n", num);
+                else
+                    Console.Write("{0} is not a prime number\n", num);
 	
 
-                //bool isprime = false;
-                //Console.WriteLine("Enter any number");
-                //int prime = Convert.ToInt32(Console.ReadLine());
-                //int i = 0;
-                //for (i = 2; i <= prime - 1; i++)
-                //{
-                //    if (prime % i == 0)
-                //    {
-                //        isprime = true;
-                //        break;
-                //    }
-                //}
-                //if (i == prime)
-                //{
-                //    isprime = true; 
-                //}
-                //Console.WriteLine(isprime);
             }
+                //creating file
+            else if (input==9)
+            {
+                string url = "demo1.txt";
+                FileStream fs = File.Create(url);
+            }
+            //Reading from file
+            else if(input==10)
+            {
+
+                FileStream fs=new FileStream("demo1.txt",FileMode.Open,FileAccess.Read);
+                StreamReader sr=new StreamReader(fs);
+                string str = sr.ReadLine();
+                while(str!=null)
+                {
+                    Console.WriteLine("{0}",str);
+                    str = sr.ReadLine();
+                }
+                sr.Close();
+                fs.Close();
+            }
+                //writing in file
+            else if (input==11)
+            {
+                FileInfo finfo = new FileInfo("demo1.txt");
+                //using (StreamWriter writer = finfo.AppendText())
+                //{
+                //    writer.WriteLine("New File with various Text operations");
+                //}
+                //finfo = new FileInfo("demo1.txt");
+                using (StreamWriter writer = finfo.CreateText())
+                {
+                    writer.WriteLine("New File asdasdsawith various Text operations");
+                }
+                using (StreamReader reader = finfo.OpenText())
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
 
 
+                //FileInfo finfo = new FileInfo("demo1.txt");
+                //StreamWriter sw = finfo.CreateText();
+                //sw.WriteLine("THis will be add from the file ");
+                //StreamReader reader = finfo.OpenText();
+                
+                //    Console.WriteLine(reader.ReadToEnd());
+                
+            }
             goto start;
 
             Console.ReadKey();
